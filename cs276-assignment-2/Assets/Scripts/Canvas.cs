72de11440baa82aa3a671f2096bc6106bbcea7de
PetrosB123 <pnblankenstein@willamette.edu>
2025-09-26 17:05:23 -0700

    v1

--- cs276-assignment-2/Assets/Scripts/Canvas.cs
+++ cs276-assignment-2/Assets/Scripts/Canvas.cs
@@ -11,42 +11,34 @@ public class Canvas : MonoBehaviour
     [SerializeField] GameObject heart;
     GameObject player;
     private float heartCount;
-    private float x = 60f;
-    private float y = 579.7501f - 60;
-    GameObject heart1;
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
     List<GameObject> heartsList = new List<GameObject> { };
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.Find("Player");
-        UpdateHearts();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (heartCount != player.GetComponent<Move>().hearts)
-        {
-            if (heartsList.Count() > 0)
-            {
-                for (int i = 0; i < heartsList.Count(); i++)
-                {
-                    Destroy(heartsList[i]);
-                }
-                heartsList.Clear();
-            }
-            UpdateHearts();
-        }
-    }
-
-    void UpdateHearts()
-    {
+        heart1.SetActive(false);
+        heart2.SetActive(false);
+        heart3.SetActive(false);
         heartCount = player.GetComponent<Move>().hearts;
-        for (int i = 0; i < heartCount; i++)
+        if (heartCount >= 1)
         {
-            heart1 = Instantiate(heart, new Vector3(x + 110 * i, y, 0), Quaternion.identity, parent: this.transform);
-            heartsList.Add(heart1);
+            heart1.SetActive(true);
+        }
+        if (heartCount >= 2)
+        {
+            heart2.SetActive(true);
+        }
+        if (heartCount >= 3)
+        {
+            heart3.SetActive(true);
         }
     }
 }
